bcdf56d5dd5e4886739da07ebc4ef0d2c4f18d49
sunmin <593691695@qq.com>
2016-12-09 13:43:25 +0800

    随便存一下，点击修改有反应

--- goodsmanage.js
+++ goodsmanage.js
@@ -1,15 +1,38 @@
 jQuery(document).ready(function() {
+		
 	$("#addgoods").click(function() {
-
-		OpenWindow = window.open("", "newwin", "height=250, width=250,toolbar=no ,scrollbars=" + scroll + ",menubar=no");
-		//写成一行   
-		OpenWindow.document.write("<TITLE>例子</TITLE>")
-		OpenWindow.document.write("<BODY BGCOLOR=#ffffff>")
-		OpenWindow.document.write("<h1>Hello!</h1>")
-		OpenWindow.document.write("New window opened!")
-		OpenWindow.document.write("</BODY>")
-		OpenWindow.document.write("</HTML>")
-		OpenWindow.document.close()
+		$("div").show();
 	})
+	$("#cancel").click(function() {
+		$("div").hide();
+	})
+	$("#addGoods").click(function() {
+		$("div").hide();
+		var shopName = $("#shopName").val();
+		var goodsName = $("#goodsName").val();
+		var noVal = $("#last").prev().children().first().text();
+		var no = parseInt(noVal) + 1;
+		var addInfor = "<tr><td>" + no + "</td><td>" + shopName + "</td><td>" + goodsName + "</td><td>" + "修改" + "</td></tr>";
+		$("#last").before(addInfor);
+	})
+	$("#search").click(function() {
+		var shopNameSear = $("#shopNameSearch").val();
+		var goodsNameSear = $("#goodsNameSearch").val();
+		var i = 0;
+		if (i == 0) {
+			$("tr").slice(3).hide();
+			i++;
+		}
+		if (i == 1) {
+			$("tr:contains('" + shopNameSear + "')").show();
+			i--;
+		}
 
+	})
+$(".imagetable tr").each(function() {
+		$(this).find("td").eq(3).click(function() {
+			console.log("hello");
+		})
+
+	})
 })
